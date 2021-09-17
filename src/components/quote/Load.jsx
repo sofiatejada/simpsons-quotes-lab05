2c1d7ac6512d9832f9b62dd2be86d482ce46290d
Sofia Tejada <sofianais@icloud.com>
2021-09-17 16:45:42 -0700

    going well so far? i think?

--- src/components/quote/Load.jsx
+++ src/components/quote/Load.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Load() {
+export default function Load({ onClick }) {
   return (
     <div>
-      
+      <button onClick={onClick} aria-label="get a quote button">
+        Get a quote!
+      </button>
     </div>
-  )
+  );
 }
+
+Load.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
